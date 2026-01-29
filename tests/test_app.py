65d7e422d5eaf6b5087bbbc6bb57b5bac146a713
Rajkumar Jain <raji@ikano.se>
2026-01-29 11:32:29 +0100

    fix the break

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,7 +13,7 @@ class TestMath:
         assert add(-1, -1) == -2
 
     def test_multiply(self):
-        assert multiply(9, 9) == 18
+        assert multiply(9, 9) == 
         assert multiply(-1, -1) == 1
         assert multiply(-1, 1) == -1
 
